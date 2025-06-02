b475d0b02854a54acf19b251efe0a04d990e4523
haqoffl <ananthametals14@gmail.com>
2025-06-02 19:01:05 +0530

    overall function is completed

--- contracts/SwappyBox/SwappBox.sol
+++ contracts/SwappyBox/SwappBox.sol
@@ -3,32 +3,63 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 
-contract Box{
-    uint public deadline;
-    uint public initialTime;
-    bool public  isContractFunded;
+
+contract Box {
+    uint256 public deadline;
+    uint256 public initialTime;
     address payable public owner;
+    bool public isContractFunded;
+    bool public isWithdraw = false;
 
-    function deposit(uint _deadline) payable public{
-        require(isContractFunded == false,"contract already funded");
-        initialTime = block.timestamp;
-        deadline = _deadline;
-        isContractFunded = true;
+    event Deposited(address indexed from, uint256 amount, uint256 deadline);
+    event Withdrawn(address indexed to, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function deposit(uint256 _durationInSeconds) external payable {
+        require(!isContractFunded, "Contract already funded");
+        require(msg.value > 0, "Must send ETH");
+        require(_durationInSeconds > 0, "Invalid duration");
+
         owner = payable(msg.sender);
+        initialTime = block.timestamp;
+        deadline = block.timestamp + _durationInSeconds;
+        isContractFunded = true;
+
+        emit Deposited(msg.sender, msg.value, deadline);
     }
 
-    function withdraw() public {
-        require(owner == msg.sender,"you are not owner");
+    function withdraw(address _to, bool isObligation) external onlyOwner {
+        require(block.timestamp >= deadline, "Deadline not yet reached");
+
         uint256 amount = address(this).balance;
-        (bool success,) = owner.call{value:amount}("");
-        require(success,"withdraw failed!");
+        require(amount > 0 && isWithdraw==false, "No funds to withdraw");
+        
+        if(isObligation == true){
+            (bool success, ) = payable(_to).call{value: amount}("");
+            require(success, "Withdraw failed");
+            isWithdraw = true;
+            emit Withdrawn(owner, amount);
+        }else{
+            uint extendDeadline = deadline+(3600 *24);
+            require(extendDeadline < block.timestamp);
+            (bool success, ) = payable(_to).call{value: amount}("");
+            require(success, "Withdraw failed");
+            isWithdraw = true;
+            emit Withdrawn(owner, amount);
+
+        }
+
 
     }
 
-    function changeOwner(address _newOwner)public {
-        owner = payable(_newOwner);
-    }
+    // Fallback to receive ETH
+    receive() external payable {}
 }
+
 contract SwappyBox{
 
     Box public box;
@@ -39,8 +70,10 @@ contract SwappyBox{
         uint basePrice;
         uint lastPrice;
         address currentWinner;
+        address poolInitiator;
     }
 
+    uint constant SLOPE = 0.3 * 1e18;
   
     mapping(address=>address[]) public boxes;
     mapping(address=>BidTrack) public boxData;
@@ -52,24 +85,69 @@ contract SwappyBox{
 
     function depositToBox(uint _index, uint _deadline) public payable {
         require(_index < boxes[msg.sender].length, "Invalid box index");
-
+        require(msg.value > 0, "deposit must be greater than 0");
         address boxAddress = boxes[msg.sender][_index];
-        Box(boxAddress).deposit{value: msg.value}(_deadline);
+        Box(payable(boxAddress)).deposit{value: msg.value}(_deadline);
+        uint256 basePrice = (msg.value * 10) / 100;
+        boxData[boxAddress] = BidTrack(block.timestamp,block.timestamp+3600,0,basePrice,basePrice,msg.sender,msg.sender);
     }
 
-    function withdraw() public{
-
+    function obligate(address _contract) public payable{
+        require(boxData[_contract].currentWinner == msg.sender,"you are not bid winner");
+        uint depositedAmount = address(_contract).balance;
+        uint paidBidAmount = boxData[_contract].basePrice;
+        uint amountToTransfer = depositedAmount - paidBidAmount;
+        require(amountToTransfer == msg.value,"obligation reverted");
+        (bool success,)=payable(boxData[_contract].poolInitiator).call{value:amountToTransfer}(""); 
+        require(success,"failed");
+        Box(payable(_contract)).withdraw(msg.sender,true);
     }
 
-    function bid() public{
+    function bid(address _contract,uint bidAmount) public payable{
+        require(boxData[_contract].currentWinner != msg.sender,"you already own");
+        uint bestPrice = getPrice(_contract);
+        if(bestPrice <= bidAmount && boxData[_contract].lastPrice < bidAmount){
+            require(msg.value == bidAmount);
+            uint initiatorFees = (bidAmount * 1)/100;
+            uint refund = bidAmount - initiatorFees;
+            (bool success,)=payable(boxData[_contract].currentWinner).call{value:refund}(""); 
+            require(success,"failed");
+            (bool success2,)=payable(boxData[_contract].poolInitiator).call{value:initiatorFees}(""); 
+            require(success2,"failed");
+            boxData[_contract].currentWinner = msg.sender;
+            boxData[_contract].lastPrice = bidAmount;
+            boxData[_contract].totalBid++;
+            boxData[_contract].bidEndTime = block.timestamp+1500;
+        
 
+        }
+
+        require(boxData[_contract].bidEndTime < block.timestamp);
+        boxData[_contract].totalBid = 0;
     }
+    
+    function withdraw(address _contract) public{
+        address initiator = boxData[_contract].poolInitiator;
+        require(initiator == msg.sender,"you are not initiator");
+        Box(payable(_contract)).withdraw(msg.sender,false);
 
-    function getPrice() public{
+    } 
 
+
+    function getPrice(address _contract) public view returns(uint256){
+        uint demand = boxData[_contract].totalBid;
+        return SLOPE * demand + boxData[_contract].basePrice;
     }
 
     
 
-}
+    function getBoxData(address _contract) public view returns(BidTrack memory){
+        return boxData[_contract];
+    }
 
+
+    function getBalance(address _contract) public view returns(uint){
+        return address(_contract).balance;
+    }
+
+}
