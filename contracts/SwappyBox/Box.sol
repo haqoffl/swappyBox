52e1af5d9a2129a01c211626976ab4c646dc6b95
haqoffl <ananthametals14@gmail.com>
2025-06-15 23:39:02 +0530

    new push of swap

--- contracts/SwappyBox/Box.sol
+++ contracts/SwappyBox/Box.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-
+import "./IERC20.sol";
 contract Box {
     uint256 public deadline;
     uint256 public initialTime;
@@ -9,8 +9,10 @@ contract Box {
     bool public isContractFunded;
     bool public isWithdraw = false;
     uint public strikePrice;
+    address public depositedToken;
 
-    event Deposited(address indexed from, uint256 amount, uint256 deadline);
+    event DepositedNative(address indexed from, uint256 amount, uint256 deadline);
+    event DepositedToken(address indexed token,address indexed from, uint256 amount, uint256 deadline);
     event Withdrawn(address indexed to, uint256 amount);
 
     modifier onlyOwner() {
@@ -28,7 +30,7 @@ contract Box {
         deadline = block.timestamp + _durationInSeconds;
         isContractFunded = true;
         strikePrice = _strikeInUSDC;
-        emit Deposited(msg.sender, msg.value, deadline);
+        emit DepositedNative(msg.sender, msg.value, deadline);
     }
 
     function withdraw(address _to, bool isObligation) external onlyOwner {
@@ -61,6 +63,44 @@ contract Box {
             return strikePrice;
         }
 
+    function depositERC20(address _token,uint _amount, uint _deadline,address from) external{
+        require(isContractFunded==false,"contract funded already");
+        require(_amount > 0, "Must send ETH");
+        require(_deadline > 0, "Invalid duration");
+        
+        IERC20(_token).transferFrom(from,address(this),_amount);
+        initialTime = block.timestamp;
+        deadline = block.timestamp+_deadline;
+        isContractFunded = true;
+        strikePrice = 1 *1e18;
+        owner = payable(msg.sender);
+        depositedToken = _token;
+        emit DepositedToken(_token,from,_amount, _deadline);
+
+    }
+
+    function WithdrawnERC20 (address to,bool isObligation) external onlyOwner {
+        require(block.timestamp >= deadline, "Deadline not yet reached");
+
+        uint256 amount = IERC20(depositedToken).balanceOf(address(this));
+        require(amount > 0 && isWithdraw==false, "No funds to withdraw");
+
+        if(isObligation == true){
+        IERC20(depositedToken).transfer(to,amount);
+            isWithdraw = true;
+            emit Withdrawn(owner, amount);
+        }else{
+            uint extendDeadline = deadline+(3600 *24);
+            require(extendDeadline < block.timestamp);
+            IERC20(depositedToken).transfer(to,amount);
+            isWithdraw = true;
+            emit Withdrawn(owner, amount);
+
+        }
+    }
+
     // Fallback to receive ETH
     receive() external payable {}
+
 }
+
--- contracts/SwappyBox/SwappBox.sol
+++ contracts/SwappyBox/SwappBox.sol
@@ -24,14 +24,20 @@ contract SwappyBox{
   
     mapping(address=>address[]) public boxes;
     mapping(address=>BidTrack) public boxData;
+    address[] public allBoxes;
     constructor(address _token){
         tokenForObligation = _token;
     }
     function createYourBox() public {
         Box bx = new Box();
         boxes[msg.sender].push(address(bx));
+        allBoxes.push(address(bx));
         emit BoxEvent(address(bx),msg.sender,boxes[msg.sender].length-1);
 
+    }
+
+    function getAllBoxes() public view returns (address[] memory){
+        return allBoxes;
     }
 
     function depositToBox(uint _index, uint _deadline,uint _strikeInUSDC) public payable {
@@ -43,12 +49,37 @@ contract SwappyBox{
         boxData[boxAddress] = BidTrack(block.timestamp,block.timestamp+3600,0,basePrice,basePrice,msg.sender,msg.sender);
     }
 
+    function erc20DepositToBox(uint _index, uint _deadline,uint _amount,address _token) public{
+        require(_index < boxes[msg.sender].length,"Invalid box index");
+        require(_amount > 0);
+        
+        Box bx = Box(payable(boxes[msg.sender][_index]));
+        bx.depositERC20(_token, _amount, _deadline,msg.sender);
+        uint priceOfToken =  1e18;
+        uint256 basePrice = (priceOfToken * 10) / 100;
+        boxData[address(bx)] = BidTrack(block.timestamp,block.timestamp+3600,0,basePrice,basePrice,msg.sender,msg.sender);
+
+
+    }
+
     function obligate(address _contract) public {
         require(boxData[_contract].currentWinner == msg.sender,"you are not bid winner");
         uint amountToTransfer = Box(payable(_contract)).getStrikePriceToPay();
-        IERC20(tokenForObligation).transferFrom(msg.sender,boxData[_contract].poolInitiator,amountToTransfer);
+        uint balance = address(_contract).balance;
+        IERC20(tokenForObligation).transferFrom(msg.sender,boxData[_contract].poolInitiator,amountToTransfer * (balance/1e18));
         Box(payable(_contract)).withdraw(msg.sender,true);
     }
+
+    function obligateERC20(address _contract,address token) public payable{
+        require(boxData[_contract].currentWinner == msg.sender,"you are not bid winner");
+        uint balance = IERC20(token).balanceOf(_contract);
+        uint wndToTransfer = Box(payable(_contract)).getStrikePriceToPay() * (balance/1e18);
+        (bool success,) = payable(boxData[_contract].poolInitiator).call{value:wndToTransfer}("");
+        require(success,"failed");
+        Box(payable(_contract)).WithdrawnERC20(msg.sender,true);
+
+    }
+
 
     function bid(address _contract) public payable{
         require(boxData[_contract].currentWinner != msg.sender,"you already own");
@@ -82,6 +113,11 @@ contract SwappyBox{
         Box(payable(_contract)).withdraw(msg.sender,false);
 
     } 
+    function withdrawERC20(address _contract) public{
+        address initiator = boxData[_contract].poolInitiator;
+        require(initiator == msg.sender,"you are not initiator");
+        Box(payable(_contract)).WithdrawnERC20(msg.sender,false);
+    }
 
 
     function getPrice(address _contract) public view returns(uint256){
@@ -97,6 +133,8 @@ contract SwappyBox{
     function getBalance(address _contractAdd) public view returns(uint){
         return address(_contractAdd).balance;
     }
+
+     
 
 }
 
